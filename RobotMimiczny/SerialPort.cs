0a84dac5eee30601bb3b2d661aecd9533813ed93
Wqwerty <johnglen@wp.pl>
2014-10-31 21:03:08 +0100

    Troche zmian w kodzie zgodnie z ustaleniami

--- RobotMimiczny/SerialPort.cs
+++ RobotMimiczny/SerialPort.cs
@@ -16,6 +16,41 @@ namespace Komunikacja
         string handshake = "none";
         string baudRate = "9600";
 
+        public int setDataBits(int newBits)
+        {
+            dataBits = Convert.ToString(newBits);
+            return 0;
+        }
+        public int setParity(string newParity)
+        {
+            parity = newParity;
+            return 0;
+        }
+        public int setStopBits(int newStopBits)
+        {
+            stopBits = Convert.ToString(newStopBits);
+            return 0;
+        }
+        public int setHandshake(string newHandshake)
+        {
+            handshake = newHandshake;
+            return 0;
+        }
+        public int setbaudRate(int newBaudRate)
+        {
+            baudRate = Convert.ToString(newBaudRate);
+            return 0;
+        }
+        public int defaultSet()
+        {
+            dataBits = "8";
+            parity = "none";
+            stopBits = "1";
+            handshake = "none";
+            baudRate = "9600";
+            return 0;
+        }
+
         // Inicjalizacja połączenia
         // Funkcja bez parametrów
         // Zwraca nazwę portu lub komunikat "Nie ma urządzenia"
@@ -30,15 +65,15 @@ namespace Komunikacja
             _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
             _serialPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
 
-            name = SetPortName();
+            name = FindPortName();
             if (name != "Nie ma urządzenia")    //sprawdzanie czy odnaleziono odpowiedni port 
             {
                 _serialPort.PortName = name;
 
                 _serialPort.ReadTimeout = 500;  //czasy oczekiwania
                 _serialPort.WriteTimeout = 500;
-                _serialPort.NewLine = "B";      //sprawdzić czy ustawiono odpowiedni znak konca lini ->ustawić zgodnie z elektronikami
-                _serialPort.Open(); 
+                _serialPort.NewLine = "$A0";      //sprawdzić czy ustawiono odpowiedni znak konca lini ->ustawić zgodnie z elektronikami
+                _serialPort.Open();
             }
             return name;
         }
@@ -54,10 +89,10 @@ namespace Komunikacja
         // Odczyt danych
         // Funkcja bez parametrów
         // Zwraca odczytaną linie lub error ("brak odpowiedzi")
-        public string Read()
+        public string Read(int iloscZapytan)
         {
-            int iloscZapytan = 0;
-            string message="brak odpowiedzi";
+            int i = iloscZapytan;
+            string message = "brak odpowiedzi";
             _continue = true;
             while (_continue)
             {
@@ -66,10 +101,9 @@ namespace Komunikacja
                     message = _serialPort.ReadLine();
                 }
                 catch (TimeoutException) { }
-                iloscZapytan++;
-                if (iloscZapytan == 3)
+                i--;
+                if (i == 0)
                 {
-                    iloscZapytan = 0;
                     _continue = false;
                 }
             }
@@ -79,7 +113,7 @@ namespace Komunikacja
         // Wyszukiwanie portu pod które podpięte jest urządzenie
         // Funkcja bez parametrów
         // Zwraca nazwę portu lub komunikat "Nie ma urządzenia"
-        public static string SetPortName()
+        public static string FindPortName()
         {
             string portName = "Nie ma urządzenia";
             int iloscZapytan = 0;
@@ -121,21 +155,97 @@ namespace Komunikacja
             return portName;
         }
 
+        public string[] SetPortName()
+        {
+            string[] portNames = new string[20];
+            int j = 0;
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                if (j < 20)
+                {
+                    portNames[j] = s;
+                    Console.WriteLine(s);
+                    Console.WriteLine(portNames[j]);
+                    j++;
+                }
+
+            }
+            return portNames;
+        }
+
+
+
         // Wysyłanie danych
         // "set"        - Wektor danych typu int
         // "commandNr"  - Numer Komendy
+        //              - 0-7 -> zapis min 1-8
+        //              - 9 -> zapis 8 min na raz
+        //              - 10 -> tryb RUN
         // Zwraca zero
-        public int Send(int[] set, int commandNr)
+        public int sendFace(int[][] sets, int commandNr)
         {
+            string[] command = { "0A", "0B", "0C", "0D", "0F", "10", "11", "AA", "1A" };
+            int blad = 0;
+
             _serialPort.NewLine = "$";              //zmiana znaku konca lini na czas wysyłania w celu zachowania formatu ramki
-            _serialPort.WriteLine("$FF$01$AF$00$0A");       //format ramki ->ustawić zgodnie z elektronikami
-            _serialPort.WriteLine(Convert.ToString(commandNr));
-            for (int i = 0; i < set.Length; i++)
+            _serialPort.WriteLine("$FF$01");       //format ramki ->ustawić zgodnie z elektronikami
+
+            for (int j = 0; j < sets.GetLength(1); j++)
             {
-                _serialPort.WriteLine(Convert.ToString(set[i]));
+                _serialPort.NewLine = "$";
+                if (commandNr == 10)
+                {
+                    _serialPort.WriteLine(command[j]);
+                }
+                else
+                {
+                    _serialPort.WriteLine(command[commandNr]);
+                }
+
+                for (int i = 0; i < sets.GetLength(0); i++)
+                {
+                    if (i == sets.GetLength(0) - 1)
+                    {
+                        _serialPort.NewLine = "$A0";            //powrót do starego znaku konca lini ->ustawić zgodnie z elektronikami
+                    }
+                    _serialPort.WriteLine(Convert.ToString(sets[j][i]));
+                }
             }
-            _serialPort.NewLine = "/x$";            //powrót do starego znaku konca lini ->ustawić zgodnie z elektronikami
-            return 0;
+            if (Read(1) == "brak odpowiedzi")
+            {
+                blad = 1;
+            }
+            return blad;
+        }
+
+        public string[] downloadFace()
+        {
+            string[] faceSets = new string[10];
+            string[] blad = new string[10];
+
+            _serialPort.WriteLine("$FF$01$01");
+            for (int i = 0; i < 8; i++)
+            {
+                faceSets[i] = Read(1);
+                if (faceSets[i] == "brak odpowiedzi")
+                {
+                    blad[0] = "brak odpowiedzi";
+                    return blad;
+                }
+            }
+            return faceSets;
+        }
+
+        public int HAI()
+        {
+            int blad = 1;
+            _serialPort.WriteLine("$FF$01$A0");       //format ramki ->ustawić zgodnie z elektronikami
+            if (Read(3) == "FF01FFA0")
+            {
+                blad = 0;
+            }
+            return blad;
         }
     }
 }
