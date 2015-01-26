58a10d30b707e30ed88bad800f9612a57b26c47b
Łukasz Rdzeń <byk777@gmail.com>
2015-01-26 10:57:34 +0100

    Final version
    program gotowy i działa

--- RobotMimiczny/Program.cs
+++ RobotMimiczny/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*! 
+ *  \brief     Program do obsługi Robota Mimicznego. PWr Mechtronika 2 sem. MGR pod kierunkiem dr hab. inż., prof. nadzw. PWr Zbigniewa Zimniaka
+ *  \author    inż. Paweł Duda 188192
+ *  \author    inż. Jan Karwasiński 188124
+ *  \author    inż. Agnieszka Nowaczyńska 188143
+ *  \author    inż. Maciej Pałka 188197
+ *  \author    inż. Łukasz Rdzeń 188175
+ *  \author    inż. Michał Sarnowski 188199
+ *  \date      rok akademicki 2014/2015
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
